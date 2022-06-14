94ad373dea509379b0e63ee8a306d82c563471a5
33180KobayashiTakugi <33180@ojs.ac.jp>
2022-06-14 15:19:00 +0900

    ScoreCounter

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+
+
 
 namespace Test01 {
     class ScoreCounter {
@@ -7,35 +10,44 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-            public ScoreCounter(string filepath) {
-                _score = ReadScore(filepath);
+             {
+                _score = ReadScore(filePath);
             }
-
-
 
         }
 
+        internal object GetPerStudentScore() {
+            throw new NotImplementedException();
+        }
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+           
+                List<Student> score = new List<Student>();
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines) {
+                    string[] items = line.Split(',');
+                Student student = new Student {
+                        Name = items[0],
+                        Subject = items[1],
+                        Score = int.Parse(items[2])
+                    };
+                    score.Add(student);
+                }
+            return score;
+            }
         }
-
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
-
-
-
-
-            
+        var dict = new Dictionary<string, int>();
+        foreach (var score in _score) {
+            if (dict.ContainsKey(score.Name))
+                dict[score.Name] += score.Score;
+            else
+                dict[score.Name] = score.Score;
         }
+        return dict;
     }
 }
+        
