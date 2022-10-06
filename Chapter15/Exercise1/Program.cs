c60334cd04930be65b0de3d617a3e5a3aa34258a
33180KobayashiTakugi <33180@ojs.ac.jp>
2022-10-06 15:58:33 +0900

    Exercise 5まで

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -26,20 +26,53 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_2() {
-
+            var max = Library.Books.Max(b => b.Price);
+            var book = Library.Books.First(b => b.Price == max);
+                Console.WriteLine(book);
+            
         }
 
 
         private static void Exercise1_3() {
-        
+            var groups = Library.Books.GroupBy(b => b.PublishedYear)
+                                      .OrderBy(g => g.Key);
+            foreach (var g in groups.OrderBy(b=>b.Key)) {
+                Console.WriteLine($"{g.Key}年 {g.Count()}冊");
+            }                                 
         }
 
         private static void Exercise1_4() {
-        
+            var selected = Library.Books
+                                  .Join(Library.Categories,     
+                                        book => book.CategoryId, 
+                                        category => category.Id, 
+                                        (book, category) => new {
+                                            book.Title,
+                                            book.PublishedYear,
+                                            book.Price,
+                                            CategoryName= category.Name
+                                        })
+                                 .OrderByDescending(x => x.PublishedYear)
+                                 .ThenByDescending(x => x.Price);
+            foreach (var item in selected)
+                Console.WriteLine("{0}年{1}円{2}({3})",
+                                    item.PublishedYear,
+                                    item.Price,
+                                    item.Title,
+                                    item.CategoryName);
+                                
         }
 
         private static void Exercise1_5() {
-        
+            var names = Library.Books.Where(b => b.PublishedYear == 2016)
+                                     .Join(Library.Categories,
+                                           book => book.CategoryId,
+                                           category => category.Id,
+                                           (book, category) => category.Name)
+                                     .Distinct();
+            foreach (var name in names) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise1_6() {
