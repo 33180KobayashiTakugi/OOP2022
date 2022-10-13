55bf5ac0630f5e05048037ac8143105fb6aec80f
33180KobayashiTakugi <33180@ojs.ac.jp>
2022-10-13 10:34:33 +0900

    15章　演習問題

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -76,15 +76,56 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_6() {
-        
-        }
+            var books = Library.Books
+                               .Join(Library.Categories,
+                               book => book.CategoryId,
+                               category => category.Id,
+                               (book, category) => new {
+                                   book.Title,
+                                   book.PublishedYear,
+                                   book.Price,
+                                   CategoryName = category.Id
+                               })
+                    .GroupBy(x => x.CategoryName)
+                    .OrderBy(x => x.Key);
+
+            foreach (var group in books) {
+                Console.WriteLine("#{0}", group.Key);
+                foreach (var item in group) {
+                    Console.WriteLine(" " + item.Title);
+                }
+            }
+        }                      
 
         private static void Exercise1_7() {
-        
+            var categoryId = Library.Categories.Single(c => c.Name == "Develpment").Id;
+            var groups = Library.Books
+                                .Where(b => b.CategoryId == categoryId)
+                                .GroupBy(b => b.PublishedYear)
+                                .OrderBy(b => b.Key);
+            foreach (var group in groups) {
+                Console.WriteLine("#{0}年", group.Key);
+                foreach (var book in group) {
+                    Console.WriteLine(" " + book.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
-        
+            var query = Library.Categories
+                               .GroupJoin(Library.Books,
+                               c => c.Id,
+                               b => b.CategoryId,
+                               (c, b) => new {
+                                   CategoryName = c.Name,
+                                   Count = b.Count()
+                               })
+                        .Where(x => x.Count >= 4);
+            foreach (var category in query) {
+                Console.WriteLine(category.CategoryName);
+            }
+                    
+
         }
     }
 }
