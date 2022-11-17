b26e584963d41eda1869993a7e4cc96760a37855
33180KobayashiTakugi <33180@ojs.ac.jp>
2022-11-17 15:58:07 +0900

    天気form 途中

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbWeatherinfo = new System.Windows.Forms.TextBox();
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbWeatherinfo
+            // 
+            this.tbWeatherinfo.Location = new System.Drawing.Point(29, 147);
+            this.tbWeatherinfo.Multiline = true;
+            this.tbWeatherinfo.Name = "tbWeatherinfo";
+            this.tbWeatherinfo.Size = new System.Drawing.Size(739, 144);
+            this.tbWeatherinfo.TabIndex = 0;
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.Location = new System.Drawing.Point(29, 12);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(112, 36);
+            this.btWeatherGet.TabIndex = 1;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(25, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "天気概況";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(138, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "地域";
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.SystemColors.Control;
+            this.pbImage.Location = new System.Drawing.Point(452, 57);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(96, 57);
+            this.pbImage.TabIndex = 5;
+            this.pbImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 683);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btWeatherGet);
+            this.Controls.Add(this.tbWeatherinfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeatherinfo;
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbImage;
+    }
+}
+
